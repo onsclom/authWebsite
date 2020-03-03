025c6bf017208782a9591c0698ec5300414ade16
Austin Merrick <austinmerrick98@gmail.com>
2020-03-03 00:05:06 -0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const volleyball = require('volleyball');
+
+const app = express();
+
+// const auth = require('./auth/index.js');
+// const auth = require('./auth/index');
+const auth = require('./auth');
+
+app.use(volleyball);
+app.use(express.json());
+app.use("/", express.static(__dirname + "/public"));
+
+/*
+app.get('/', (req, res) => {
+  res.json({
+    message: 'wow. hello world!'
+  });
+});
+*/
+
+app.use('/auth', auth);
+
+function notFound(req, res, next) {
+  res.status(404);
+  const error = new Error('Not Found - ' + req.originalUrl);
+  next(error);
+}
+
+function errorHandler(err, req, res, next) {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log('Listening on port', port);
+});
